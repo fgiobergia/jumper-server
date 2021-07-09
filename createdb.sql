abe584f4bad80e119a72cfc2758c78d066394c03
Flavio Giobergia <flavio.giobergia@gmail.com>
2021-07-09 18:30:43 +0200

    moving test queries to separate sql file

--- createdb.sql
+++ createdb.sql
@@ -4,8 +4,6 @@ create table sessions(
     id varchar(64) primary key,
     user_id varchar(64),
     creation timestamp,
+    closed boolean default false,
     foreign key(user_id) references users(id)
 ); 
-
--- test user
-insert into users values('c6354001bc5d6538dd856d00aa25a2458e44fd3b9214d4bad2dc06278f6b059f');
---
+++ testsetup.sql
@@ -0,0 +1,2 @@
+-- test user
+insert into users values('c6354001bc5d6538dd856d00aa25a2458e44fd3b9214d4bad2dc06278f6b059f');
